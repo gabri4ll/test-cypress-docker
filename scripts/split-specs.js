748b05993f031287af66692d3c57a41fa9a0d3d7
gabri4ll <gabriel.nascimento@madeiramadeira.com.br>
2025-01-30 13:20:33 -0300

    incluindo paralelismo

--- scripts/split-specs.js
+++ scripts/split-specs.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 const allSpecs = fs.readdirSync('./cypress/e2e').filter(file => file.endsWith('.cy.js'));
-const numWorkers = parseInt(process.env.WORKERS) || 3;
+const containerIndex = parseInt(process.argv[2]) - 1; // Começa do 1, então ajustamos
+const totalContainers = parseInt(process.argv[3]);
 
-const chunkSize = Math.ceil(allSpecs.length / numWorkers);
-const workerIndex = parseInt(process.env.WORKER_INDEX) || 0;
-const specsToRun = allSpecs.slice(workerIndex * chunkSize, (workerIndex + 1) * chunkSize);
+const chunkSize = Math.ceil(allSpecs.length / totalContainers);
+const specsToRun = allSpecs.slice(containerIndex * chunkSize, (containerIndex + 1) * chunkSize);
 
-console.log(specsToRun.join(','));
+console.log(specsToRun.map(spec => `cypress/e2e/${spec}`).join(','));
